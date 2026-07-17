dc5bc3989ad5f37113da8405231acfd5de122d79
aaronsabu22-prog <aaronsabu.22@gmail.com>
2026-07-17 10:39:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ paper_text = ""
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request=request,
+        name="index.html"
     )
 
 
